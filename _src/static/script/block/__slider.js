d94e942daeda996d79ce1eb2f4d68c8aa45a3f08
Юля <ymakarevich76@gmail.com>
2021-09-06 15:20:34 +0300

    Add slider

--- _src/static/script/block/__slider.js
+++ _src/static/script/block/__slider.js
@@ -1,7 +1,9 @@
-const swiper = new Swiper('directory__slider', {
+const swiper = new Swiper('.directory__slider', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
   slidesPerView: 5,
+  autoheight: true,
+  spaceBetween: 20,
 });
