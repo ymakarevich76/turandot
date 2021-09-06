284f66ee808588d0d16cac26e74bd1421a1c5eea
Юля <ymakarevich76@gmail.com>
2021-09-06 17:02:14 +0300

    Add goods in catalog

--- _src/static/script/block/__slider.js
+++ _src/static/script/block/__slider.js
@@ -6,4 +6,16 @@ const swiper = new Swiper('.directory__slider', {
   slidesPerView: 5,
   autoheight: true,
   spaceBetween: 20,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    992: {
+      slidesPerView: 5,
+    }
+  }
 });
