fc9d30cea33a0690b97742562c29ab2d2d4ce95a
Юля <ymakarevich76@gmail.com>
2021-09-07 12:52:12 +0300

    Correct modal for mob

--- _src/static/script/block/__contacts.js
+++
@@ -1,9 +0,0 @@
-// const iconArrow = document.querySelector('.header__arrow');
-// if (iconArrow) {
-//   const modal = document.querySelector('.modal');
-//   iconArrow.addEventListener('click', (e) => {
-//     document.body.classList.toggle('lock');
-//     iconArrow.classList.toggle('active');
-//     modal.classList.toggle('active');
-//   })
-// }
--- _src/static/script/main.js
+++ _src/static/script/main.js
@@ -2,5 +2,4 @@
 
 @@include('block/__buttons-blur.js');
 @@include('block/__menu.js');
-@@include('block/__contacts.js');
 @@include('block/__slider.js');
