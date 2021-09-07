e929fdbb3a0d6271e7d8b6900f5a44f632291dfe
Юля <ymakarevich76@gmail.com>
2021-09-07 12:25:28 +0300

    Correct index.html

--- _src/static/script/main.js
+++ _src/static/script/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 @@include('block/__buttons-blur.js');
-@@include('block/__touch.js');
 @@include('block/__menu.js');
 @@include('block/__contacts.js');
 @@include('block/__slider.js');
